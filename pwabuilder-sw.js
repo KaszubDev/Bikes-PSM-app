d7954cb309648b52129e30efa1379c4ff74ba99c
Krzysztof <krzysztof@mybusinessmatches.com>
2020-04-09 22:17:10 +0200

    added new files

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -2,7 +2,8 @@
 
 const CACHE = "pwabuilder-page";
 
-const offlineFallbackPage = "index.html";
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
